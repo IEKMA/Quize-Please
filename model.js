9abcbe49f82ce834b2ce1b33e0ff5f058056bbaf
Valeriya190396 <valerinakom@mail.ru>
2024-05-03 11:27:38 +0300

    feat:Model.js

---
+++ model.js
@@ -0,0 +1,30 @@
+const { log } = require("console");
+const fs = require("fs").promises;
+
+class Model {
+  static async getQuestionsAndAnswers(path) {
+    const text = await fs.readFile(`./topics/${path}`, "utf-8");
+    const arr = text.split("\n");
+    console.log(arr);
+
+    const arrFromTxt = [];
+    for (let i = 0; i < arr.length; i++) {
+      const objFromTxt = {};
+      const question = arr[i];
+      const correctAnsw = arr[i + 1];
+      const answer = arr[i + 2];
+
+      if (question && correctAnsw) {
+        objFromTxt.question = question;
+        objFromTxt.correctAnsw = correctAnsw;
+        objFromTxt.answer = answer.split(', ');
+        arrFromTxt.push(objFromTxt)
+      }
+    } 
+    return arrFromTxt
+  }
+}
+
+console.log(Model.getQuestionsAndAnswers("nighthawk_flashcard_data.txt"));
+
+module.exports = Model;
