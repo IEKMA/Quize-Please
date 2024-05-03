b4e5deb7c3df7129a5a45ca13c1c6be70f4da607
Pavel <pvltrbn2211@gmail.com>
2024-05-03 12:06:51 +0300

    fix:Viuw

---
+++ View.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer')
+const fs = require ('fs').promises
+
+
+class View {
+    static async startQuiz() {
+        return inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'category',
+              message: 'Выберите категорию',
+              choices: [
+                'Кофе',
+                'мороженное'
+              ],
+            }
+          ]);
+      }
+      static async pullQuestions({ question, answer, choices }, counter){
+               let userAnswer = await inquirer.prompt([{
+               type: 'list',
+               name: 'userAnswer',
+               message: question,
+               choices,
+             }
+           ]);        
+         if (userAnswer.userAnswer.trim().toLowerCase() === answer.toLowerCase()) {
+            console.log('Правильно');
+            counter += 10;
+          } else {
+            console.log(('Не верно'));
+            console.log(`Правильный ответ: ${answer}`);
+          }
+          return counter;
+        }
+    }
+module.exports = View
