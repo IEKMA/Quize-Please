7bed939a15cea968cb5c316984be3be5647cbdac
Pavel <pvltrbn2211@gmail.com>
2024-05-03 11:23:51 +0300

    пробник

---
+++ View.js
@@ -0,0 +1,37 @@
+const inquirer = require ('inquirer')
+
+
+class View {
+    static async startQuiz() {
+        return inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'category',
+              message: 'Выберите категорию',
+              choices: [
+                'Кофе',
+                'Вторая'
+              ],
+            }
+          ]);
+      }
+      static async pullQuestions({ question, answer, choices }, counter){
+               let userAnswer = await inquirer.prompt([{
+               type: 'list',
+               name: 'userAnswer',
+               message: question,
+               choices,
+             }
+           ]);        
+         if (userAnswer.userAnswer.trim().toLowerCase() === answer.toLowerCase()) {
+            console.log(chalk.green('Правильно'));
+            counter += 10;
+          } else {
+            console.log(chalk.red('Не верно'));
+            console.log(`Правильный ответ: ${chalk.green(answer)}`);
+          }
+          return counter;
+        }
+    }
+module.exports = View
